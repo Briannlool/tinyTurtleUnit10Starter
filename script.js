1394ba7fff26292f9a519e41d250436ee6575bd0
Brian Chan <brian.chan080@gmail.com>
2017-02-14 22:03:36 +0000

    house

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+/* global TinyTurtle */ 
+/* global forward */ 
+/* global right */ 
+/* global stamp */ 
 TinyTurtle.apply(window);
 
+/*
 function square(){
 forward(50);
 right(90);
@@ -11,8 +16,63 @@ forward(50);
 right(90);
 forward(50);
 }
-
+*/
 // Type your function call below
+/*square(); */ 
+/*
+stamp();
 
+
+
+function triangle(x){
+    forward (x);
+    right (60); 
+    forward (x); 
+    right (60); 
+    forward (x); 
+}
+
+
+ triangle (40); 
 
 stamp();
+*/ 
+function house(){
+    left (135);
+    forward (30);
+    left(135); 
+    forward (42); 
+    left (135); 
+    forward (30); 
+    right (180); 
+    forward (30); 
+    right(45); 
+    forward(20); 
+    right(90);
+    forward(40);
+    right(90);
+    forward (20);
+    
+    
+    
+    
+}
+  house();
+stamp (); 
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
